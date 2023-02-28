8bb06716b465c6d0d4e474a3b964c4165272ba96
aanmeba <aanmeba@gmail.com>
2023-02-28 20:33:47 +1100

    remove aws s3 related code on the top side

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,16 +1,16 @@
 class Appearance < ApplicationRecord
   belongs_to :user
-  has_one_attached :picture, service: :amazon
+  # has_one_attached :picture, service: :amazon
 
   # input data validation for those who don't use frontend input fields
   validates :profile_title, length: {maximum: 30}, presence: true
   validates :bio, length: {maximum: 80}, presence: true
 
-  before_save :picture_url
+  # before_save :picture_url
   
-  def picture_url
-    picture.blob.url if picture.attached?
-  end
+  # def picture_url
+  #   picture.blob.url if picture.attached?
+  # end
 
   def self.find_by_user_id(username)
     user_id = User.find_by_username(username).id
